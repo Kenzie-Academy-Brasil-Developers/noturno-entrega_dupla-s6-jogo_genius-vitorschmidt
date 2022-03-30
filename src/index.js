a005286cde5858ff03f5a4e0ee5c27f803784b92
andre <andrepassoni98@gmail.com>
2022-03-30 16:56:12 -0300

    feat: array with random colors

--- src/index.js
+++ src/index.js
@@ -2,3 +2,15 @@ const getRandomColor = () => {
   const colors = ["red", "green", "blue", "yellow"];
   return colors[Math.floor(Math.random() * colors.length)];
 };
+
+const updateColor = (array) => {
+  array.push(getRandomColor());
+};
+
+const button = document.querySelector("button");
+const state = [];
+
+button.addEventListener("click", () => {
+  updateColor(state);
+  console.log(state);
+});
