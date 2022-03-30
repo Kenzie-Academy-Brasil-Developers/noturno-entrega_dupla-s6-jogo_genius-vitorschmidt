2fbacfad0bee89d95b276f19c94f2baff1fc2e80
andre <andrepassoni98@gmail.com>
2022-03-30 17:10:52 -0300

    fix: passed game to a controller

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
-const getRandomColor = () => {
-  const colors = ["red", "green", "blue", "yellow"];
-  return colors[Math.floor(Math.random() * colors.length)];
-};
-
-const updateColor = (array) => {
-  array.push(getRandomColor());
-};
-
+import Game from "./controllers/Game.js";
 const button = document.querySelector("button");
-const state = [];
 
 button.addEventListener("click", () => {
-  updateColor(state);
-  console.log(state);
+  Game.updateColor(Game.actualState);
+  console.log(Game.actualState);
 });
